20e0abcf51e26d2d472e19f0833e940922ec3d0e
Bangash Owais （忠勇-李） <63273140+Bangash514@users.noreply.github.com>
2023-04-25 17:01:15 +0800

    Update slicer.cpp

--- slicer.cpp
+++ slicer.cpp
@@ -1,5 +1,5 @@
-Multi-planar medical image reconstruction is a complex topic that involves many different algorithms and techniques. While it is possible to create a basic 
-demo in C++, the full implementation of a multi-planar medical image reconstruction system would require significant time and resources.That being said, 
+Multi-planar medical image reconstruction is a complex topic that involves many different algorithms and techniques. While it is possible to create a basic
+demo in C++, the full implementation of a multi-planar medical image reconstruction system would require significant time and resources.That being said,
 here is an example of how to use the ITK library in C++ to perform multi-planar reconstruction:
 
 #include <itkImage.h>
